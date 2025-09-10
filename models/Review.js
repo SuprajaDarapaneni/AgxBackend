b45aee6115279b9d5daa816ecd0104ba914709ce
yaswanthkumarch <yaswanthkumarch2001@gmail.com>
2025-09-10 22:06:13 +0530

    updated image

--- models/Review.js
+++ models/Review.js
@@ -6,6 +6,7 @@ const reviewSchema = new mongoose.Schema({
   rating: { type: Number, required: true, min: 1, max: 5 },
   comment: { type: String, required: true },
   date: { type: Date, default: Date.now },
+    imageUrl: { type: String, default: null }, 
 });
 
 export default mongoose.model('Review', reviewSchema);
